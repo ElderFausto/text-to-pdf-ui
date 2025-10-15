4c0fbc001f2c4094d7d5d750fcc61f28b85c1cac
elderfausto <elderfavsto@gmail.com>
2025-10-15 12:05:50 -0300

    chore: project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    
+    provideHttpClient(),
+    importProvidersFrom(FormsModule)
   ]
 };
