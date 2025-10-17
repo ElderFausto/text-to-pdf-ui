9355bd8cd8626903441f5300d0293b6e99e09126
elderfausto <elderfavsto@gmail.com>
2025-10-17 18:56:21 -0300

    feat: add url backend aws

--- src/app/pdf.ts
+++ src/app/pdf.ts
@@ -3,18 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PdfService {
   // URL backend local
-  private apiUrl = 'http://localhost:8080/api/pdf/generate';
+  // private apiUrl = 'http://localhost:8080/api/pdf/generate';
 
-  constructor(private http: HttpClient) { }
+  // URL deployada no AWS Elastic Beanstalk
+  private apiUrl = 'http://text-to-pdf.sa-east-1.elasticbeanstalk.com/';
+
+  constructor(private http: HttpClient) {}
 
   generatePdf(text: string): Observable<Blob> {
     return this.http.post(this.apiUrl, text, {
       headers: { 'Content-Type': 'text/plain' },
-      responseType: 'blob'
+      responseType: 'blob',
     });
   }
 }
