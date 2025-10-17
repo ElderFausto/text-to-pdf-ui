258b31a79226338adf682b7912c54d75df97912a
elderfausto <elderfavsto@gmail.com>
2025-10-17 19:48:53 -0300

    feat: new update API endpoint to AWS Elastic Beanstalk URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://text-to-pdf-env-1.sa-east-1.elasticbeanstalk.com//api'
+  apiUrl: 'http://text-to-pdf-env-1.sa-east-1.elasticbeanstalk.com/api'
 };
