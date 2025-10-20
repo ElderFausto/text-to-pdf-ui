6fda951421d6df7aea826f5fbf6bea704ce32155
elderfausto <elderfavsto@gmail.com>
2025-10-20 12:23:54 -0300

    chore: Revert recent changes related to deployment configuration

--- src/app/pdf.ts
+++ src/app/pdf.ts
@@ -1,32 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class PdfService {
   // URL backend local
-  // private apiUrl = 'http://localhost:8080/api/pdf/generate';
-
-  // URL deployada no AWS Elastic Beanstalk
-  private apiUrl = `${environment.apiUrl}/api/pdf/generate`;
+  private apiUrl = 'http://localhost:8080/api/pdf/generate';
 
   constructor(private http: HttpClient) {}
 
-  generatePdf(htmlContent: string): Observable<Blob> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'text/html', // Mudamos para text/html no passo anterior
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': '0'
-    });
-
-    return this.http.post(this.apiUrl, htmlContent, {
-      headers: headers, 
-      responseType: 'blob'
+  generatePdf(text: string): Observable<Blob> {
+    return this.http.post(this.apiUrl, text, {
+      headers: { 'Content-Type': 'text/plain' },
+      responseType: 'blob',
     });
   }
 }
--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://text-to-pdf-env-1.sa-east-1.elasticbeanstalk.com'
-};
